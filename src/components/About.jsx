3c68e22ef9159c555c205a02f858492d1d7dc2fc
mhroohi <mhossein.roohi@gmail.com>
2023-06-14 15:52:55 -0600

    wip

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,19 +30,18 @@ const About = () => {
   return (
     <div className="-mt-[6rem]">
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
+        <p className={styles.sectionSubText}>Who amd I?</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
       <motion.p
         variants={fadeIn('', '', 0.1, 1)}
         className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
-        sapiente ipsum dolorum dicta eaque cumque inventore molestias, beatae ea
-        quaerat alias accusamus voluptas autem! Alias odit voluptates in totam
-        vitae dignissimos minus eaque culpa unde tempore dolore aperiam
-        obcaecati voluptatum aliquam corrupti, suscipit accusamus! Odit unde
-        veniam dolorum ipsum doloribus.
+        I'm Mohammad Hossein Roohi, a Data Scientist specializing in anomaly detection and behavior analysis. 
+        With a Ph.D. in Electrical Engineering from the University of Alberta, 
+        my research focused on industrial alarm management systems and causality analysis. Currently, 
+        I work as a Data Scientist, focusing on anomaly detection and behavior analysis. 
+        Through statistical modeling and machine learning, I uncover insights and provide actionable intelligence.
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
